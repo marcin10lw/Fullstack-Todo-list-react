a14ed767381b7bbfd297771b26c1f9a76cf29e43
marcin10lw <marcin10lw@wp.pl>
2022-12-15 15:21:58 +0100

    moved useTasks to separate file

--- src/App.js
+++ src/App.js
@@ -5,54 +5,7 @@ import Buttons from "./Buttons";
 import Header from "./Header";
 import Section from "./Section";
 import Main from "./Main";
-
-const useTasks = () => {
-  const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || []
-  );
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  const completeAll = () => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        return {
-          ...task,
-          done: true,
-        };
-      })
-    );
-  };
-
-  const toggleDone = (id) => {
-    setTasks((tasks) =>
-      tasks.map((task) =>
-        task.id === id ? { ...task, done: !task.done } : task
-      )
-    );
-  };
-
-  const deleteTask = (id) => {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
-  };
-
-  const addNewTask = (newTask) => {
-    setTasks((tasks) => {
-      return [
-        ...tasks,
-        {
-          content: newTask,
-          done: false,
-          id: tasks.length === 0 ? 1 : tasks.length + 1,
-        },
-      ];
-    });
-  };
-
-  return [tasks, completeAll, toggleDone, deleteTask, addNewTask];
-};
+import { useTasks } from "./useTasks";
 
 function App() {
   const [tasks, completeAll, toggleDone, deleteTask, addNewTask] = useTasks();
---
+++ src/useTasks.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+const useTasks = () => {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const completeAll = () => {
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        return {
+          ...task,
+          done: true,
+        };
+      })
+    );
+  };
+
+  const toggleDone = (id) => {
+    setTasks((tasks) =>
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
+
+  const addNewTask = (newTask) => {
+    setTasks((tasks) => {
+      return [
+        ...tasks,
+        {
+          content: newTask,
+          done: false,
+          id: tasks.length === 0 ? 1 : tasks.length + 1,
+        },
+      ];
+    });
+  };
+
+  return [tasks, completeAll, toggleDone, deleteTask, addNewTask];
+};
+
+export {useTasks};
