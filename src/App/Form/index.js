ca34a348b6d9c6add8139dc8f1fea19ff95d09a0
marcin10lw <marcin10lw@wp.pl>
2022-12-16 13:59:31 +0100

    removed redundant React import

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { StyledForm, NewTask, Button} from "./styled";
 
 const Form = ({addNewTask}) => {
