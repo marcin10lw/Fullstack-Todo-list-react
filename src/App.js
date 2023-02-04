6ea4587eb2e9db9d5e441693a70c77118d4b89e0
marcin10lw <marcin10lw@wp.pl>
2023-02-04 18:03:13 +0100

    created NotFoundPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./common/Navbar";
+import NotFoundPage from "./common/NotFoundPage";
 import AuthorPage from "./features/author/AuthorPage";
 import TaskPage from "./features/tasks/TaskPage";
 import TasksPage from "./features/tasks/TasksPage";
@@ -12,7 +13,7 @@ function App() {
         <Route path="/zadania" element={<TasksPage />} />
         <Route path="/zadania/:id" element={<TaskPage />} />
         <Route path="/autor" element={<AuthorPage />} />
-        {/* <Route path="*" element={<AuthorPage />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/common/NotFoundPage/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Container from "../Container/styled";
+import { NotFoundPageSection, GoBackLink } from "./styled";
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/zadania");
+    }, 7000);
+  }, []);
+
+  return (
+    <Container>
+      <NotFoundPageSection>
+        <p>Ups...Nie znaleziono takiej podstrony 😥</p>
+        <p>Za chwilę przekierujemy Cię do z powrotem do zadań 😄</p>
+        <p>
+          <GoBackLink to="/zadania">Wróć do zadań</GoBackLink>
+        </p>
+      </NotFoundPageSection>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
