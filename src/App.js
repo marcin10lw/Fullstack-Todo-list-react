4681a5f6e35f4a7bc0082d234852d1f9759b3ff0
marcin10lw <marcin10lw@wp.pl>
2023-02-04 16:24:17 +0100

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./common/Navbar";
+import AuthorPage from "./features/author/AuthorPage";
 import TasksPage from "./features/tasks/TasksPage";
 
 function App() {
-  return <TasksPage />;
+  return (
+    <BrowserRouter basename="To-do-list-react">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/autor" element={<AuthorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
