a788f620014067ea0f1fea83f5e19040b28eefb0
marcin10lw <marcin10lw@wp.pl>
2023-02-16 22:10:05 +0100

    styled NotesArea

---
+++ src/features/tasks/TaskPage/NotesArea/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledNotesArea = styled.textarea`
+  resize: vertical;
+  border-radius: 5px;
+  border: 1.5px solid ${({ theme }) => theme.colors.lightGrey};
+  outline: none;
+  background-color: transparent;
+  width: 100%;
+  height: 200px;
+  padding: 7px 10px;
+
+  &:focus {
+    outline: 1px solid ${({ theme }) => theme.colors.teal};
+  }
+`;
