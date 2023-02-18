3e14652e975bf838210099b06dbddea32c7d1349
marcin10lw <marcin10lw@wp.pl>
2023-02-18 10:50:47 +0100

    created a controlled input in NotesArea

--- src/features/tasks/TaskPage/NotesArea/index.js
+++ src/features/tasks/TaskPage/NotesArea/index.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Wrapper } from "../../../../common/Wrapper";
+import { addNoteContent, selectTasks } from "../../tasksSlice";
 import { StyledNotesArea } from "./styled";
 
-const NotesArea = () => {
+const NotesArea = ({ task }) => {
+  const [noteInput, setNoteInput] = useState("");
+  const dispatch = useDispatch();
+
+  const onInputChange = ({ target }) => {
+    setNoteInput(target.value);
+    console.log(noteInput);
+    dispatch(addNoteContent(task, noteInput));
+  };
+
   return (
     <Wrapper>
-      <StyledNotesArea></StyledNotesArea>
+      <StyledNotesArea
+        placeholder="Wpisz swoje notatki..."
+        value={noteInput}
+        onChange={onInputChange}
+      />
     </Wrapper>
   );
 };
