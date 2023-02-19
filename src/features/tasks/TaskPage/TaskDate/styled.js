0d5921bb62e80c52dcb3bef3eecd27af0b676f79
marcin10lw <marcin10lw@wp.pl>
2023-02-19 21:39:21 +0100

    removed redundant styled.js files

--- src/features/tasks/TaskPage/TaskDate/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const StyledTaskDate = styled.span`
-  color: ${({ theme }) => theme.colors.doveGrey};
-`;
--- src/features/tasks/TaskPage/TimeLeft/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const StyledTimeLeft = styled.span`
-  color: ${({ theme }) => theme.colors.doveGrey};
-`;
