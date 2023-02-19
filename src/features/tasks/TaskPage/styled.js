89f35de01c245fcadacd3413a9a35cf2f5a11ce7
marcin10lw <marcin10lw@wp.pl>
2023-02-19 21:37:21 +0100

    styled info spans in TaskPage

--- src/features/tasks/TaskPage/styled.js
+++ src/features/tasks/TaskPage/styled.js
@@ -3,3 +3,18 @@ import styled from "styled-components";
 export const TaskPageContent = styled.div`
   padding: 20px;
 `;
+
+export const TimePassedInfo = styled.span`
+  color: ${({ theme }) => theme.colors.rose};
+  margin-right: 10px;
+`;
+
+export const TaskDoneInfo = styled.span`
+  color: ${({ theme }) => theme.colors.teal};
+  font-weight: 700;
+`;
+
+export const TaskNotDoneInfo = styled.span`
+  color: ${({ theme }) => theme.colors.hibiscus};
+  font-weight: 700;
+`;
