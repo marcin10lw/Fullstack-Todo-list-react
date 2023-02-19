8c786bc527f811deb89929064ecc2cd8913a32a8
marcin10lw <marcin10lw@wp.pl>
2023-02-19 12:43:34 +0100

    changed on focus styling of DateInput

--- src/features/tasks/TaskPage/Deadline/styled.js
+++ src/features/tasks/TaskPage/Deadline/styled.js
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 
-export const DateAndTime = styled.input`
+export const DateInput = styled.input`
   max-width: 20%;
   padding: 5px 8px;
   border: 1px solid ${({ theme }) => theme.colors.lightGrey};
   border-radius: 3px;
   color: ${({ theme }) => theme.colors.doveGrey};
+
+  &:focus {
+    outline: 1px solid ${({ theme }) => theme.colors.teal};
+  }
+`;
+
+export const Text = styled.span`
+  color: ${({ theme }) => theme.colors.doveGrey};
+  margin-right: 10px;
 `;
