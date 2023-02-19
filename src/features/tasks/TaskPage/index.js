c608139ae0baba7f9265f2ab52abe596ab759308
marcin10lw <marcin10lw@wp.pl>
2023-02-19 21:32:37 +0100

    made conditional  rendering in deadline secction

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -7,13 +7,20 @@ import Section from "../../../common/Section";
 import { selectTaskById } from "../tasksSlice";
 import Deadline from "./Deadline";
 import NotesArea from "./NotesArea";
-import { TaskPageContent } from "./styled";
+import {
+  TaskPageContent,
+  TimePassedInfo,
+  TaskDoneInfo,
+  TaskNotDoneInfo,
+} from "./styled";
 import TaskDate from "./TaskDate";
 import TimeLeft from "./TimeLeft";
+import { useTimeLeft } from "./useTimeLeft";
 
 const TaskPage = () => {
   const { id } = useParams();
   const task = useSelector((state) => selectTaskById(state, id));
+  const [timeLeft, isPassedDeadline] = useTimeLeft(task);
 
   return (
     <Container>
@@ -32,7 +39,22 @@ const TaskPage = () => {
       <Section
         header="Deadline"
         content={<Deadline task={task} />}
-        optionalContent={<TimeLeft task={task} />}
+        optionalContent={
+          !task.done &&
+          task.deadline.deadlineDate !== "" &&
+          (!isPassedDeadline ? (
+            <TimeLeft task={task} timeLeft={timeLeft} />
+          ) : (
+            <div>
+              <TimePassedInfo>Czas minął:</TimePassedInfo>
+              {task.done ? (
+                <TaskDoneInfo>Ukończono zadanie</TaskDoneInfo>
+              ) : (
+                <TaskNotDoneInfo>Nie ukończono zadania</TaskNotDoneInfo>
+              )}
+            </div>
+          ))
+        }
       />
       <Section header="Notatki" content={<NotesArea task={task} />} />
     </Container>
