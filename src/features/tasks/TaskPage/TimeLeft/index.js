a49f273739712a890e704de943e3330d85a2979c
marcin10lw <marcin10lw@wp.pl>
2023-02-19 13:30:18 +0100

    added useEffect with setInterval in TimeLeft

--- src/features/tasks/TaskPage/TimeLeft/index.js
+++ src/features/tasks/TaskPage/TimeLeft/index.js
@@ -1,19 +1,31 @@
+import { setHours } from "date-fns";
 import formatDistanceStrict from "date-fns/formatDistanceStrict";
 import pl from "date-fns/locale/pl";
+import { useEffect, useState } from "react";
 import { StyledTimeLeft } from "./styled";
 
 const TimeLeft = ({ task }) => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
   const deadline = task.deadline ? task.deadline : new Date();
+  const formattedDeadline = setHours(new Date(deadline), 24);
 
   const timeLeft = formatDistanceStrict(
-    Date.parse(task.date),
-    Date.parse(deadline),
+    Date.parse(currentDate),
+    Date.parse(formattedDeadline),
     {
       locale: pl,
       includeSeconds: true,
-      unit: "second",
     }
   );
+
+  useEffect(() => {
+    const dateIntervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(dateIntervalId);
+  }, [currentDate]);
 
   return (
     task.deadline && <StyledTimeLeft>Pozostało: {timeLeft}</StyledTimeLeft>
