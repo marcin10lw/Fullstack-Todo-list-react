b79cb320fcc79c93d291f76d5f43d22a497d8b70
marcin10lw <marcin10lw@wp.pl>
2023-02-19 12:44:00 +0100

    changed locale to pl in TaskDate

--- src/features/tasks/TaskPage/TaskDate/index.js
+++ src/features/tasks/TaskPage/TaskDate/index.js
@@ -1,11 +1,11 @@
 import { StyledTaskDate } from "./styled";
 import { format } from "date-fns";
-import { cs } from "date-fns/locale";
+import pl from "date-fns/locale/pl";
 
 const TaskDate = ({ task }) => {
   const date = Date.parse(task.date);
   const formatedDate = format(date, "eee, dd/MM/yyyy 'o' HH:mm:ss", {
-    locale: cs,
+    locale: pl,
   });
 
   return <StyledTaskDate>Utworzono: {formatedDate}</StyledTaskDate>;
