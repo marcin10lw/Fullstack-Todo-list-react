e6672b36d2f82f02221c6d95fa8a5e2aba718d7d
marcin10lw <marcin10lw@wp.pl>
2023-03-05 20:41:00 +0100

    gave InfoText align-self: flex-start in mobileview

--- src/features/tasks/TaskPage/InfoText/index.js
+++ src/features/tasks/TaskPage/InfoText/index.js
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const InfoText = styled.span`
   color: ${({ theme }) => theme.colors.doveGrey};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    align-self: flex-start;
+  }
 `;
