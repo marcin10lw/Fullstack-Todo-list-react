a2ead94b82fb1b8fa10ca5abcf368deaf635c33b
marcin10lw <marcin10lw@wp.pl>
2023-03-18 00:05:26 +0100

    created new routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
 import Navbar from "./common/Navbar";
 import NotFoundPage from "./common/NotFoundPage";
+import LoginPage from "./features/auth/LoginPage";
 import AuthorPage from "./features/author/AuthorPage";
 import TaskPage from "./features/tasks/TaskPage";
 import TasksPage from "./features/tasks/TasksPage";
@@ -10,6 +11,9 @@ function App() {
     <HashRouter>
       <Navbar />
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" />
+        <Route path="/reset" />
         <Route path="/" element={<Navigate to="/zadania" />} />
         <Route path="/zadania" element={<TasksPage />} />
         <Route path="/zadania/:id" element={<TaskPage />} />
