538996f178f0ba0a429e7e94849132e7ee73880b
marcin10lw <marcin10lw@wp.pl>
2023-03-18 13:54:00 +0100

    created register functionality

--- src/features/auth/RegisterPage/index.js
+++ src/features/auth/RegisterPage/index.js
@@ -1,5 +1,11 @@
-import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Container from "../../../common/Container/styled";
+import Loader from "../../../common/Loader";
+import { auth } from "../../../config/firebase";
 import {
   AuthButton,
   AuthForm,
@@ -10,25 +16,73 @@ import {
 } from "../AuthStyled";
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const registerUser = async (event) => {
+    event.preventDefault();
+
+    if (password !== passwordConfirm) {
+      toast.error("Passwords do not match.");
+      return;
+    }
+    setIsLoading(true);
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setIsLoading(false);
+      toast.success("Account Registered.");
+      // navigate("/zadania");
+    } catch (error) {
+      const errorCode = error.code;
+      if (errorCode === "auth/email-already-in-use") {
+        toast.error("User with this email arleady exist");
+      }
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Container auth>
-      <AuthSection>
-        <AuthHeading>Register</AuthHeading>
-        <AuthForm>
-          <AuthInput type="email" placeholder="Email..." required />
-          <AuthInput type="password" placeholder="Password..." required />
-          <AuthInput
-            type="password"
-            placeholder="Confirm Password..."
-            required
-          />
-          <AuthButton>Register</AuthButton>
-        </AuthForm>
-        <AuthMessage>
-          Already have an account? <Link to="/login">Login</Link>
-        </AuthMessage>
-      </AuthSection>
-    </Container>
+    <>
+      {isLoading && <Loader />}
+      <ToastContainer pauseOnFocusLoss={false} />
+      <Container auth>
+        <AuthSection>
+          <AuthHeading>Register</AuthHeading>
+          <AuthForm onSubmit={registerUser}>
+            <AuthInput
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              type="email"
+              placeholder="Email..."
+              required
+            />
+            <AuthInput
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              type="password"
+              placeholder="Password..."
+              required
+            />
+            <AuthInput
+              value={passwordConfirm}
+              onChange={({ target }) => setPasswordConfirm(target.value)}
+              type="password"
+              placeholder="Confirm Password..."
+              required
+            />
+            <AuthButton>Register</AuthButton>
+          </AuthForm>
+          <AuthMessage>
+            Already have an account? <Link to="/login">Login</Link>
+          </AuthMessage>
+        </AuthSection>
+      </Container>
+    </>
   );
 };
 
