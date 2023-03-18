9777037fdaf82e70c85dc69f258a4244d9d7327c
marcin10lw <marcin10lw@wp.pl>
2023-03-18 12:00:18 +0100

    added elements to register and reset paths

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
 import Navbar from "./common/Navbar";
 import NotFoundPage from "./common/NotFoundPage";
 import LoginPage from "./features/auth/LoginPage";
+import RegisterPage from "./features/auth/RegisterPage";
+import ResetPage from "./features/auth/ResetPage";
 import AuthorPage from "./features/author/AuthorPage";
 import TaskPage from "./features/tasks/TaskPage";
 import TasksPage from "./features/tasks/TasksPage";
@@ -12,8 +14,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" />
-        <Route path="/reset" />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reset" element={<ResetPage />} />
         <Route path="/" element={<Navigate to="/zadania" />} />
         <Route path="/zadania" element={<TasksPage />} />
         <Route path="/zadania/:id" element={<TaskPage />} />
