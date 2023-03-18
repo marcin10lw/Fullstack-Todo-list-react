84a7b5d7316134115ce7380698e5cdbc383914af
marcin10lw <marcin10lw@wp.pl>
2023-03-18 23:13:58 +0100

    added reset password functionality

--- src/features/auth/ResetPage/index.js
+++ src/features/auth/ResetPage/index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import Container from "../../../common/Container/styled";
+import Loader from "../../../common/Loader";
+import { auth } from "../../../config/firebase";
 import {
   AuthButton,
   AuthForm,
@@ -11,20 +15,50 @@ import {
 import { FlexWrapper } from "./styled";
 
 const ResetPage = () => {
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const resetPassword = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setIsLoading(false);
+      toast.success("Check email reset password.");
+      navigate("/login");
+    } catch (error) {
+      setIsLoading(false);
+      if (error.code === "auth/user-not-found") {
+        toast.error("Account with this email doesn't exist");
+      }
+    }
+  };
+
   return (
-    <Container auth>
-      <AuthSection>
-        <AuthHeading>Reset Password</AuthHeading>
-        <AuthForm>
-          <AuthInput type="email" placeholder="Email..." required />
-          <AuthButton>Reset Password</AuthButton>
-        </AuthForm>
-        <FlexWrapper>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </FlexWrapper>
-      </AuthSection>
-    </Container>
+    <>
+      {isLoading && <Loader />}
+      <Container auth>
+        <AuthSection>
+          <AuthHeading>Reset Password</AuthHeading>
+          <AuthForm onSubmit={resetPassword}>
+            <AuthInput
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              type="email"
+              placeholder="Email..."
+              required
+            />
+            <AuthButton>Reset Password</AuthButton>
+          </AuthForm>
+          <FlexWrapper>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </FlexWrapper>
+        </AuthSection>
+      </Container>
+    </>
   );
 };
 
