3a25fb82cab6bcb1c61311c4011c934f03ffb494
marcin10lw <marcin10lw@wp.pl>
2023-03-18 23:13:45 +0100

    added onAuthStateChanged in useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./common/Navbar";
 import NotFoundPage from "./common/NotFoundPage";
 import LoginPage from "./features/auth/LoginPage";
@@ -9,11 +9,50 @@ import TaskPage from "./features/tasks/TaskPage";
 import TasksPage from "./features/tasks/TasksPage";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  removeActiveUser,
+  selectIsLoggedIn,
+  setActiveUser,
+} from "./features/auth/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const emailUserName = user.email
+          .split("@")[0]
+          .match(/[a-zA-Z0-9]+/g)
+          .join("");
+
+        dispatch(
+          setActiveUser({
+            email: user.email,
+            userName: user.displayName ? user.displayName : emailUserName,
+            userId: user.uid,
+          })
+        );
+      } else {
+        dispatch(removeActiveUser());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <>
-      <ToastContainer pauseOnFocusLoss={false} pauseOnHover={false} />
+      <ToastContainer
+        autoClose={2500}
+        limit={3}
+        pauseOnFocusLoss={false}
+        pauseOnHover={false}
+      />
       <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
