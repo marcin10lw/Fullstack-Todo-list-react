9477285c6f6d189cf25f20865a2620a3d49905bb
marcin10lw <marcin10lw@wp.pl>
2023-03-18 13:53:54 +0100

    created login functionality

--- src/features/auth/LoginPage/index.js
+++ src/features/auth/LoginPage/index.js
@@ -10,28 +10,74 @@ import {
   AuthButton,
   AuthMessage,
 } from "../AuthStyled";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../config/firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "../../../common/Loader";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loginUser = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setIsLoading(false);
+      toast.success("Logged In.");
+    } catch (error) {
+      const errorCode = error.code;
+      if (errorCode === "auth/wrong-password") {
+        toast.error("Wrong email or password.");
+      } else if (errorCode === "auth/user-not-found") {
+        toast.error("This account does not exist");
+      }
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Container auth>
-      <AuthSection>
-        <AuthHeading>Login</AuthHeading>
-        <AuthForm>
-          <AuthInput type="email" placeholder="Email..." required />
-          <AuthInput type="password" placeholder="Password..." required />
-          <AuthButton>Login</AuthButton>
-        </AuthForm>
-        <Text>
-          <Link to="/reset">Forgot Password?</Link>
-        </Text>
-        <AuthButton google>
-          <AiOutlineGoogle size={20} /> Login With Google
-        </AuthButton>
-        <AuthMessage>
-          Don't have an account? <Link to="/register">Register</Link>
-        </AuthMessage>
-      </AuthSection>
-    </Container>
+    <>
+      {isLoading && <Loader />}
+      <ToastContainer />
+      <Container auth>
+        <AuthSection>
+          <AuthHeading>Login</AuthHeading>
+          <AuthForm onSubmit={loginUser}>
+            <AuthInput
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              type="email"
+              placeholder="Email..."
+              required
+            />
+            <AuthInput
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              type="password"
+              placeholder="Password..."
+              required
+            />
+            <AuthButton>Login</AuthButton>
+          </AuthForm>
+          <Text>
+            <Link to="/reset">Forgot Password?</Link>
+          </Text>
+          <AuthButton google>
+            <AiOutlineGoogle size={20} /> Login With Google
+          </AuthButton>
+          <AuthMessage>
+            Don't have an account? <Link to="/register">Register</Link>
+          </AuthMessage>
+        </AuthSection>
+      </Container>
+    </>
   );
 };
 
