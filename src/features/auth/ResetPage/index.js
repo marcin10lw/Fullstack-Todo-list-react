66016197148a8aebc6b32de12f916fec5f4d87f7
marcin10lw <marcin10lw@wp.pl>
2023-03-20 18:17:11 +0100

    used setIsLoading in ResetPage

--- src/features/auth/ResetPage/index.js
+++ src/features/auth/ResetPage/index.js
@@ -1,10 +1,11 @@
 import { sendPasswordResetEmail } from "firebase/auth";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Container from "../../../common/Container/styled";
-import Loader from "../../../common/Loader";
 import { auth } from "../../../config/firebase";
+import { setIsLoading } from "../authSlice";
 import {
   AuthButton,
   AuthForm,
@@ -16,20 +17,21 @@ import { FlexWrapper } from "./styled";
 
 const ResetPage = () => {
   const [email, setEmail] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const resetPassword = async (event) => {
     event.preventDefault();
-    setIsLoading(true);
 
+    dispatch(setIsLoading(true));
     try {
       await sendPasswordResetEmail(auth, email);
-      setIsLoading(false);
       toast.success("Check email reset password.");
       navigate("/login");
+      dispatch(setIsLoading(false));
     } catch (error) {
-      setIsLoading(false);
+      dispatch(setIsLoading(false));
       if (error.code === "auth/user-not-found") {
         toast.error("Account with this email doesn't exist");
       }
@@ -37,28 +39,25 @@ const ResetPage = () => {
   };
 
   return (
-    <>
-      {isLoading && <Loader />}
-      <Container auth>
-        <AuthSection>
-          <AuthHeading>Reset Password</AuthHeading>
-          <AuthForm onSubmit={resetPassword}>
-            <AuthInput
-              value={email}
-              onChange={({ target }) => setEmail(target.value)}
-              type="email"
-              placeholder="Email..."
-              required
-            />
-            <AuthButton>Reset Password</AuthButton>
-          </AuthForm>
-          <FlexWrapper>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </FlexWrapper>
-        </AuthSection>
-      </Container>
-    </>
+    <Container auth>
+      <AuthSection>
+        <AuthHeading>Reset Password</AuthHeading>
+        <AuthForm onSubmit={resetPassword}>
+          <AuthInput
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            type="email"
+            placeholder="Email..."
+            required
+          />
+          <AuthButton>Reset Password</AuthButton>
+        </AuthForm>
+        <FlexWrapper>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </FlexWrapper>
+      </AuthSection>
+    </Container>
   );
 };
 
