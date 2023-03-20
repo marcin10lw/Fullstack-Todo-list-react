c19213bb8ba0613ae6d0f3edb26680a7e52b62af
marcin10lw <marcin10lw@wp.pl>
2023-03-20 16:54:10 +0100

    moved onAuthStateChanged to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import TodoList from "./features/TodoList";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { removeActiveUser, setActiveUser } from "./features/auth/authSlice";
+import { createNameFromEmail } from "./features/auth/createNameFromEmail";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { auth } from "./config/firebase";
+import { setTasks } from "./features/tasks/tasksSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          setActiveUser({
+            email: user.email,
+            userName: user.displayName
+              ? user.displayName
+              : createNameFromEmail(user.email),
+            userId: user.uid,
+          })
+        );
+
+        navigate("/tasks");
+      } else {
+        dispatch(removeActiveUser());
+        dispatch(setTasks([]));
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
     <>
       <ToastContainer
--- src/features/TodoList.js
+++ src/features/TodoList.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "../common/Navbar";
 import NotFoundPage from "../common/NotFoundPage";
 import LoginPage from "./auth/LoginPage";
@@ -7,47 +7,9 @@ import ResetPage from "./auth/ResetPage";
 import AuthorPage from "./author/AuthorPage";
 import TaskPage from "./tasks/TaskPage";
 import TasksPage from "./tasks/TasksPage";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../config/firebase";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  removeActiveUser,
-  selectIsLoggedIn,
-  setActiveUser,
-} from "./auth/authSlice";
 import RequireAuth from "../common/RequireAuth";
-import { createNameFromEmail } from "./auth/createNameFromEmail";
-import { setTasks } from "./tasks/tasksSlice";
 
 function TodoList() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        dispatch(
-          setActiveUser({
-            email: user.email,
-            userName: user.displayName
-              ? user.displayName
-              : createNameFromEmail(user.email),
-            userId: user.uid,
-          })
-        );
-      } else {
-        dispatch(removeActiveUser());
-        dispatch(setTasks([]));
-      }
-    });
-  }, [dispatch, navigate]);
-
-  useEffect(() => {
-    isLoggedIn ? navigate("/tasks") : navigate("/login");
-  }, [isLoggedIn]);
-
   return (
     <>
       <Navbar />
