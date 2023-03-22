ac249f7675e24027425905467b4fea37b9a71763
marcin10lw <marcin10lw@wp.pl>
2023-03-22 19:32:17 +0100

    added goback link in header and styled header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,12 +1,17 @@
 import { useLocation } from "react-router-dom";
-import { Heading } from "./styled";
+import { Heading, GoBackLink, GoBackIcon } from "./styled";
 
-const Header = ({ heading }) => {
+const Header = ({ heading, optionalContent }) => {
   const { pathname } = useLocation();
 
   return (
     <header>
       <Heading inHome={pathname === "/"}>{heading}</Heading>
+      {optionalContent && (
+        <GoBackLink to="/tasks">
+          <GoBackIcon />
+        </GoBackLink>
+      )}
     </header>
   );
 };
