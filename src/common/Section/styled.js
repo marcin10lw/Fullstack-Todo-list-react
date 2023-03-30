9921e8004a969797fcb2c6db7b4d2f8d558d0ecb
marcin10lw <marcin10lw@wp.pl>
2023-03-30 11:07:25 +0200

    gave Section border-radius of 3px

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -4,6 +4,7 @@ export const StyledSection = styled.section`
   display: flex;
   flex-direction: column;
   margin-bottom: 10px;
+  border-radius: 3px;
   box-shadow: 2px 2px 7px 0px hsl(0, 0%, 68%);
   background-color: white;
 `;
