060b2171b7dd995f548c94526c1a28420d8d55ca
marcin10lw <marcin10lw@wp.pl>
2023-03-31 12:45:32 +0200

    moved useStorage to features

---
+++ src/features/useStorage.js
@@ -0,0 +1,53 @@
+import { serverTimestamp } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
+import { useEffect } from "react";
+import { v4 } from "uuid";
+import { auth, storage } from "../config/firebase";
+import { addFirebaseDoc } from "./tasks/firebaseFunctions";
+
+const useStorage = (file, folder, taskId) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(false);
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    if (!file) return;
+
+    const uniqueName = `${file.name}${v4()}`;
+    const storageRef = ref(storage, `${folder}/${uniqueName}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progress);
+      },
+      (error) => {
+        setError(true);
+      },
+      async () => {
+        const url = await getDownloadURL(uploadTask.snapshot.ref);
+        setUrl(url);
+        taskId &&
+          (await addFirebaseDoc(
+            {
+              url,
+              taskId,
+              userId: auth.currentUser.uid,
+              createdAt: serverTimestamp(),
+              name: uniqueName,
+            },
+            "images"
+          ));
+      }
+    );
+  }, [file]);
+
+  return { progress, error, url };
+};
+
+export default useStorage;
