c44e75c5aeb13de0033b10bb5a030363835c92eb
marcin10lw <marcin10lw@wp.pl>
2023-04-14 22:42:37 +0200

    added mobile view to Carousel

--- src/features/tasks/TaskPage/ImagesList/Carousel/styled.js
+++ src/features/tasks/TaskPage/ImagesList/Carousel/styled.js
@@ -5,5 +5,10 @@ export const CarouselImage = styled.img`
   max-width: 90%;
   box-shadow: 0 0 20px #1b2525;
   border: 3px solid #565f5f;
-  z-index: 9999;
+  z-index: 999;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    max-height: 80%;
+    max-width: 80%;
+  }
 `;
