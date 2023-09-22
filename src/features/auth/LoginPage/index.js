d703d5e001fdcfa8ab0972710a8e70004263c5b5
marcin10lw <marcin10lw@wp.pl>
2023-09-22 15:07:09 +0200

    created demo user

--- src/features/auth/LoginPage/index.js
+++ src/features/auth/LoginPage/index.js
@@ -1,12 +1,8 @@
 import Container from "../../../common/Container/styled";
 import { AiOutlineGoogle } from "react-icons/ai";
-import { CredentialsInfo, DescriptionListGroup, Text } from "./styled";
+import { Text } from "./styled";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import { auth, provider } from "../../../config/firebase";
-import { toast } from "react-toastify";
-import { addUserToDatabase } from "../addUserToDatabase";
 import {
   AuthSection,
   AuthHeading,
@@ -15,107 +11,57 @@ import {
   AuthButton,
   AuthMessage,
 } from "../AuthStyled";
-import { useDispatch } from "react-redux";
-import { setIsLoading } from "../authSlice";
-import { AiOutlineCopy } from "react-icons/ai";
-import { credentialsList } from "./credentialsList";
+import useFirebaseLogin from "./service";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const dispatch = useDispatch();
-
-  const loginUser = async (event) => {
-    event.preventDefault();
-
-    dispatch(setIsLoading(true));
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      dispatch(setIsLoading(false));
-      toast.success("You're Logged In.");
-    } catch (error) {
-      dispatch(setIsLoading(false));
-      const errorCode = error.code;
-      if (errorCode === "auth/wrong-password") {
-        toast.error("Wrong email or password.");
-      } else if (errorCode === "auth/user-not-found") {
-        toast.error("This account does not exist");
-      }
-    }
-  };
-
-  const signInWithGoogle = async () => {
-    dispatch(setIsLoading(true));
-
-    try {
-      const { user } = await signInWithPopup(auth, provider);
-      toast.success("You're Logged In.");
-
-      addUserToDatabase(user);
-      dispatch(setIsLoading(false));
-    } catch (error) {
-      dispatch(setIsLoading(false));
-    }
-  };
-
-  const onCredentialCopy = async (text) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      toast.success("Copied do clipboard", { position: "bottom-center" });
-    } catch (error) {
-      toast.error("Couldn't copy to clipboard", { position: "bottom-center" });
-    }
-  };
+  const { loginUser, signInWithGoogle } = useFirebaseLogin();
 
   return (
-    <>
-      <Container auth>
-        <AuthSection>
-          <CredentialsInfo>
-            <p>To test the application use this credentials:</p>
-            <dl>
-              {credentialsList.map(({ id, type, content }) => (
-                <DescriptionListGroup key={id}>
-                  <dt>{type}:</dt>
-                  <dd>{content}</dd>
-                  <button onClick={() => onCredentialCopy(content)}>
-                    <AiOutlineCopy />
-                  </button>
-                </DescriptionListGroup>
-              ))}
-            </dl>
-          </CredentialsInfo>
-          <AuthHeading>Login</AuthHeading>
-          <AuthForm onSubmit={loginUser}>
-            <AuthInput
-              value={email}
-              onChange={({ target }) => setEmail(target.value)}
-              type="email"
-              placeholder="Email..."
-              required
-            />
-            <AuthInput
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-              type="password"
-              placeholder="Password..."
-              required
-            />
-            <AuthButton>Login</AuthButton>
-          </AuthForm>
-          <Text>
-            <Link to="/reset">Forgot Password?</Link>
-          </Text>
-          <AuthButton onClick={signInWithGoogle} google>
-            <AiOutlineGoogle size={20} /> Sign In With Google
+    <Container auth>
+      <AuthSection>
+        <AuthHeading>Login</AuthHeading>
+        <AuthForm onSubmit={() => loginUser({ email, password })}>
+          <AuthInput
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            type="email"
+            placeholder="Email..."
+            required
+          />
+          <AuthInput
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            type="password"
+            placeholder="Password..."
+            required
+          />
+          <AuthButton>Login</AuthButton>
+          <AuthButton
+            type="button"
+            onClick={() =>
+              loginUser({
+                email: "test_app@test.com",
+                password: "Testing123!",
+              })
+            }
+          >
+            Demo User
           </AuthButton>
-          <AuthMessage>
-            Don't have an account? <Link to="/register">Register</Link>
-          </AuthMessage>
-        </AuthSection>
-      </Container>
-    </>
+        </AuthForm>
+        <Text>
+          <Link to="/reset">Forgot Password?</Link>
+        </Text>
+        <AuthButton onClick={signInWithGoogle} google>
+          <AiOutlineGoogle size={20} /> Sign In With Google
+        </AuthButton>
+        <AuthMessage>
+          Don't have an account? <Link to="/register">Register</Link>
+        </AuthMessage>
+      </AuthSection>
+    </Container>
   );
 };
 
